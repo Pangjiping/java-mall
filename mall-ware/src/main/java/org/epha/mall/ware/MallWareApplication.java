e0a5a0c89afcc92079fd4968edebeb9367830e63
Pangjiping <13626376642@163.com>
2023-03-23 17:28:16 +0800

    project struct init

---
+++ mall-ware/src/main/java/org/epha/mall/ware/MallWareApplication.java
@@ -0,0 +1,13 @@
+package org.epha.mall.ware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallWareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallWareApplication.class, args);
+    }
+
+}
